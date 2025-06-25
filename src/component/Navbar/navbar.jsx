53de36cc594a5e2d568d9fb2498155db2f825243
Ibkakj <ibk.akeju@gmail.com>
2025-06-25 20:36:24 +0100

    trial 2

--- src/component/Navbar/navbar.jsx
+++ src/component/Navbar/navbar.jsx
@@ -3,11 +3,7 @@ import React from "react";
 const navbar = () => {
   return (
     <div className="flex flex-1 justify-between">
-      <img
-        src="public\Group 33968.png"
-        alt="logo"
-        className="w-[150px] h-[38px]"
-      />
+      <img src="/Group 33968.png" alt="logo" className="w-[150px] h-[38px]" />
       <button className="w-[150px] p-2 border-[2px] rounded-full items-center">
         Join Waitlist
       </button>
